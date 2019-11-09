cb306992483b131d23ac71aae55d729da36d265b
Tim <timirwing@gmail.com>
2019-11-09 11:12:52 +0300

    setup product app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    >
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{ product }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+  },
+  data() {
+    return {
+      product: 'dsadas',
+    };
   },
 };
 </script>
@@ -24,8 +22,6 @@ export default {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
         color: #2c3e50;
-        margin-top: 60px;
     }
 </style>
