c61a9c15f601d730ea9a5919526250644ae4cc47
Tim <timirwing@gmail.com>
2019-11-11 20:34:04 +0300

    create product component and refactor its insides

--- src/App.vue
+++ src/App.vue
@@ -1,264 +1,63 @@
 <template>
   <div id="app">
-    <section class="product">
-      <div class="product-image">
-        <img
-          :src="product.imageSrc"
-          alt="Socks">
-      </div>
-
-      <div class="product-info">
-        <h1>
-          {{ name }}
-          <span class="sale" v-show="product.onSale">On Sale!</span>
-        </h1>
-
-        <p>
-          <span v-show="inventory > 25">
-            In Stock
-          </span>
-          <span v-show="inventory <= 25 && inventory > 0">
-            Almost sold out!
-          </span>
-          <span v-show="inventory <= 0">
-            Out of Stock
-          </span>
-        </p>
-
-        <ul>
-          <li
-            v-for="detail in details"
-            :key="detail">
-            {{ detail }}
-          </li>
-        </ul>
-
-
-        <ul class="buttonList">
-          <li v-for="(variant, index) in variants"
-              :key="variant.id">
-
-            <label class="colorBtn"
-                   :aria-label="variant.color">
-              <input type="radio"
-                     name="color"
-                     class="visuallyhidden"
-                     :checked="variant.color === product.color"
-                     @change="updateProduct(index)">
-              <span class="colorBtn__inside" :style="{'background-color': variant.bgColor}"></span>
-            </label>
-          </li>
-        </ul>
-
-        <ul class="buttonList">
-          <li
-            v-for="(count, key) in product.sizes"
-            :key="key">
-            <label>
-               <input type="radio" name="size" @change="size = key" :checked="size === key">
-              {{ key }}
-            </label>
-          </li>
-        </ul>
-        <p>{{ modelCount }} pieces available</p>
-
-        <button class="toCart"
-                v-on:click="addToCart"
-                :disabled="!product.sizes[size]">
-          Add to Cart
-        </button>
-
-        <div class="cart">
-          <p>Cart ({{cart.length}})</p>
-        </div>
-      </div>
-    </section>
+    <nav class="nav-bar"></nav>
+    <Product :product="product"/>
   </div>
 </template>
 
 <script>
+import Product from './components/Product.vue';
+
 import greenSocks from './assets/greenSocks.jpg';
 import blueSocks from './assets/blueSocks.jpg';
 
 export default {
   name: 'App',
   components: {
+    Product,
   },
   data() {
     return {
-      details: ['80% cotton', '20% polyester', 'Unisex'],
-      name: 'Socks',
-      selected: 0,
-      size: '',
-      variants: [
-        {
-          id: 2234,
-          color: 'mint green',
-          bgColor: '#359264',
-          imageSrc: greenSocks,
-          onSale: true,
-          sizes: {
-            XS: 0, S: 3, M: 7, L: 4, XL: 2,
+      product: {
+        name: 'Socks',
+        details: ['80% cotton', '20% polyester', 'Unisex'],
+        variants: [
+          {
+            id: 2234,
+            colorDesc: 'mint green',
+            bgColor: '#359264',
+            imageSrc: greenSocks,
+            onSale: true,
+            sizes: {
+              XS: 0, S: 3, M: 7, L: 4, XL: 2,
+            },
           },
-        },
-        {
-          id: 2235,
-          color: 'dark blue',
-          bgColor: '#405267',
-          imageSrc: blueSocks,
-          onSale: false,
-          sizes: {
-            XS: 2, S: 6, M: 15, L: 0, XL: 9,
+          {
+            id: 2235,
+            colorDesc: 'dark blue',
+            bgColor: '#405267',
+            imageSrc: blueSocks,
+            onSale: false,
+            sizes: {
+              XS: 2, S: 6, M: 15, L: 0, XL: 9,
+            },
           },
-        },
-      ],
-      cart: [],
+        ],
+      },
     };
-  },
-  computed: {
-    modelCount() {
-      const count = this.product.sizes[this.size];
-      return count !== undefined ? count : this.inventory;
-    },
-    product() {
-      return this.variants[this.selected];
-    },
-    inventory() {
-      return Object.keys(this.product.sizes).reduce((acc, key) => acc + this.product.sizes[key], 0);
-    },
-  },
-  methods: {
-    updateProduct(index) {
-      this.selected = index;
-      this.size = '';
-    },
-    addToCart() {
-      this.cart.push(this.variants[this.selected]);
-    },
   },
 };
 </script>
 
 <style>
-    .sale {
-      color: #f3f3f3;
-      background-color: #F2341B;
-      vertical-align: baseline;
-      font-size: .8em;
-      padding: .3em .4em .1em .4em;
-      border-radius: .2em;
-      box-sizing: border-box;
-    }
-
-    body {
-      font-family: tahoma, sans-serif;
-      color:#282828;
-      margin: 0;
-    }
-
-    #app {
-      font-family: 'Avenir', Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      color: #233346;
-    }
-
-    .product {
-      display: flex;
-      flex-flow: wrap;
-      padding: 1rem;
-    }
-
-    .buttonList {
-      padding: 0;
-      display: flex;
-      list-style: none;
-    }
-
-    .buttonList > *:not(:last-child) {
-      margin-right: .5em;
-    }
-
-    img {
-      border: 1px solid #d8d8d8;
-      width: 70%;
-      margin: 40px;
-      box-shadow: 0 .5px 1px #d8d8d8;
-    }
-
-    .product-image {
-      width: 80%;
-    }
-
-    .colorBtn {
-      cursor: pointer;
-      display: block;
-      position: relative;
-      width: 3em;
-      height: 3em;
-      border: 1px solid #3336;
-      border-radius: .5em;
-    }
-
-    .colorBtn__inside {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      border-radius: .25em;
-      width: 1.25em;
-      height: 1.25em;
-    }
-
-    input[type=radio]:checked ~ .colorBtn__inside {
-      width: 2.25em;
-      height: 2.25em;
-    }
-
-    input[type=radio]:focus ~ .colorBtn__inside::after {
-      content: '';
-      position: absolute;
-      top: 50%; left: 50%;
-      transform: translate(-50%, -50%);
-      display: block;
-      width: 145%;
-      height: 145%;
-      border-radius: 15%;
-      box-sizing: border-box;
-      border: 3px solid #4fc88c;
-    }
-    .visuallyhidden {
-      position: absolute;
-      overflow: hidden;
-      clip: rect(0 0 0 0);
-      height: 1px; width: 1px;
-      margin: -1px; padding: 0; border: 0;
-    }
-
-    .cart {
-      margin-right: 25px;
-      float: right;
-      border: 1px solid #d8d8d8;
-      padding: 5px 20px;
-    }
-
-    .toCart {
-      margin-top: 30px;
-      border: none;
-      background-color: #1E95EA;
-      color: white;
-      height: 40px;
-      width: 100px;
-      font-size: 14px;
-    }
-
-    .toCart:disabled {
-      background-color: #d8d8d8;
-    }
-
-    .product-image,
-    .product-info {
-      margin-top: 10px;
-      width: 50%;
-    }
+  .nav-bar {
+    background: linear-gradient(-90deg, #84CF6A, #16C0B0);
+    height: 30px;
+  }
+  body {
+    background-color: #fefefe;
+    font-family: tahoma, sans-serif;
+    color:#282828;
+    margin: 0;
+  }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,231 @@
+<template>
+  <section class="product">
+    <div class="product-image">
+      <img :src="current.imageSrc"
+           alt="Socks">
+    </div>
+
+    <div class="product-info">
+      <h1>
+        {{ product.name }}
+        <span class="sale" v-show="current.onSale">On Sale!</span>
+      </h1>
+
+      <p>
+          <span v-show="inventory > 25">
+            In Stock
+          </span>
+        <span v-show="inventory <= 25 && inventory > 0">
+            Almost sold out!
+          </span>
+        <span v-show="inventory <= 0">
+            Out of Stock
+          </span>
+      </p>
+
+      <ul>
+        <li v-for="detail in product.details"
+            :key="detail">
+          {{ detail }}
+        </li>
+      </ul>
+
+
+      <ul class="buttonList">
+        <li v-for="(variant, index) in product.variants"
+            :key="variant.id">
+          <label class="squareButton"
+                 :aria-label="variant.colorDesc">
+            <input type="radio"
+                   name="color"
+                   class="visuallyhidden"
+                   :checked="index === 0"
+                   @change="updateProduct(index)">
+            <span class="colorBtn__inside" :style="{'background-color': variant.bgColor}"></span>
+          </label>
+        </li>
+      </ul>
+
+      <ul class="buttonList">
+        <li v-for="(_, key) in current.sizes"
+            :key="key">
+          <label class="squareButton squareButton_small">
+            <input type="radio"
+                   name="size"
+                   class="visuallyhidden"
+                   @change="size = key"
+                   :checked="size === key">
+            <span class="sizeBtn__text">{{ key }}</span>
+          </label>
+        </li>
+      </ul>
+      <p class="itemsLeft">{{ modelCount }} pieces available</p>
+
+      <button class="toCart" :disabled="!current.sizes[size]">
+        Add to Cart
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      selected: 0,
+      size: '',
+    };
+  },
+  computed: {
+    modelCount() {
+      const count = this.current.sizes[this.size];
+      return count !== undefined ? count : this.inventory;
+    },
+    current() {
+      return this.product.variants[this.selected];
+    },
+    inventory() {
+      return Object.keys(this.current.sizes).reduce((acc, key) => acc + this.current.sizes[key], 0);
+    },
+  },
+  methods: {
+    updateProduct(index) {
+      this.selected = index;
+      this.size = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+
+  .product {
+    font-size: 1.2em;
+    display: flex;
+    padding: 2em;
+
+    color: #233346;
+    font-family: 'Lato', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  .product-image,
+  .product-info {
+    width: 50%;
+  }
+  img {
+    display: block;
+    width: 66.666%;
+    margin: 2.5em auto;
+    border: 2px solid #d8d8d8;
+    border-radius: 4px;
+    box-shadow: 0 10px 4px -8px #d8d8d8;
+  }
+
+  .sale {
+    color: #fefefe;
+    background-color: #F2341B;
+    font-size: .8em;
+    font-weight: 400;
+    padding: .1em .3em .1em .3em;
+    border-radius: .2em;
+  }
+
+  .buttonList {
+    display: flex;
+    padding: 0;
+    margin: 1em 0;
+    list-style: none;
+  }
+  .buttonList > *:not(:last-child) {
+    margin-right: 1em;
+  }
+
+  .squareButton {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    box-sizing: border-box;
+    position: relative;
+    width: 3em;
+    height: 3em;
+    border: 1px solid #3336;
+    border-radius: .5em;
+    cursor: pointer;
+  }
+  .squareButton:focus-within {
+    border-color: transparent;
+    box-shadow: 0 0 0 3px #4fc88c;
+  }
+
+  .squareButton_small {
+    width: 2em;
+    height: 2em;
+  }
+
+  .colorBtn__inside {
+    width: 50%;
+    height: 50%;
+    border-radius: .333em;
+  }
+
+  input[type=radio]:checked ~ .colorBtn__inside {
+    width: 75%;
+    height: 75%;
+  }
+
+  .sizeBtn__text {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+
+  input[type=radio]:checked ~ .sizeBtn__text {
+    background-color: #4fc88c77;
+    font-size: 1.3em;
+  }
+
+  .visuallyhidden {
+    position: absolute;
+    display: block;
+    overflow: hidden;
+    height: 1px; width: 1px;
+    clip: rect(0 0 0 0);
+    left: -9999px;
+    padding: 0; border: 0;
+  }
+
+  .itemsLeft {
+    margin-left: 1em;
+  }
+
+  .toCart {
+    margin-top: 2em;
+    border: none;
+    border-radius: 2px;
+    background-color: #4e7891;
+    letter-spacing: .05em;
+    font-family: inherit;
+    color: #fefefe;
+    padding: .8em 1.2em;
+    font-size: 14px;
+  }
+  .toCart:focus {
+    outline: none;
+    box-shadow: 0 0 .3em #222;
+  }
+  .toCart:disabled {
+    background-color: #9b9b9b;
+  }
+</style>
