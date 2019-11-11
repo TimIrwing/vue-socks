1e26ef7ad95356804aee22cf705439d1531c9b1a
Tim <timirwing@gmail.com>
2019-11-11 09:57:20 +0300

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "vue/html-closing-bracket-spacing": ["error", {
       "startTag":"never",
       "endTag": "never",
-      "selfClosingTag": "always",
+      "selfClosingTag": "never",
     }],
     "vue/html-self-closing": ["error", {
       "html": {
