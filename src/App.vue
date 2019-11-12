54e86262d72a5299c67cf26372b2e3ad1b175217
Tim <timirwing@gmail.com>
2019-11-12 18:22:56 +0300

    change navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="app">
-    <nav class="nav-bar"></nav>
+    <nav class="nav">
+      <ul class="nav__content">
+        <li>
+          <h1 class="title">Vue Socks</h1>
+        </li>
+        <li>
+          <cart :cart="cart"/>
+        </li>
+      </ul>
+    </nav>
     <Product :product="product"/>
   </div>
 </template>
 
 <script>
 import Product from './components/Product.vue';
+import Cart from './components/Cart.vue';
 
 import greenSocks from './assets/greenSocks.jpg';
 import blueSocks from './assets/blueSocks.jpg';
@@ -14,6 +24,7 @@ import blueSocks from './assets/blueSocks.jpg';
 export default {
   name: 'App',
   components: {
+    Cart,
     Product,
   },
   data() {
@@ -50,14 +61,35 @@ export default {
 </script>
 
 <style>
-  .nav-bar {
+  .nav {
     background: linear-gradient(-90deg, #84CF6A, #16C0B0);
-    height: 30px;
+    padding: 1em;
+    display: flex;
+    justify-content: center;
   }
+  .nav__content {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    width: 100%;
+    max-width: 1200px;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .title {
+    margin: 0;
+    font-weight: 400;
+    font-size: 2.5em;
+    color: #26455a;
+  }
+
   body {
     background-color: #fefefe;
     font-family: tahoma, sans-serif;
-    color:#282828;
+    color:#233346;
     margin: 0;
   }
 </style>
