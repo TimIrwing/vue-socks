4bdf04f2e539d2450053080785abd627fb9c6cf4
Tim <timirwing@gmail.com>
2019-11-12 21:01:16 +0300

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,48 @@ module.exports = {
     'plugin:vue/base',
   ],
   rules: {
-    "vue/html-closing-bracket-newline": ["error", {
-      "singleline": "never",
-      "multiline": "never"
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'never'
     }],
-    "vue/html-closing-bracket-spacing": ["error", {
-      "startTag":"never",
-      "endTag": "never",
-      "selfClosingTag": "never",
+    'vue/html-closing-bracket-spacing': ['error', {
+      'startTag':'never',
+      'endTag': 'never',
+      'selfClosingTag': 'never',
     }],
-    "vue/html-self-closing": ["error", {
-      "html": {
-        "void": "never",
-        "normal": "never",
-        "component": "always"
+    'vue/html-self-closing': ['error', {
+      'html': {
+        'void': 'never',
+        'normal': 'never',
+        'component': 'always'
       },
-      "svg": "always",
-      "math": "always"
+      'svg': 'always',
+      'math': 'always'
+    }],
+    'vue/order-in-components': ['error', {
+      'order': [
+        'el',
+        'name',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'fetch',
+        'asyncData',
+        'data',
+        'computed',
+        'watch',
+        'LIFECYCLE_HOOKS',
+        'methods',
+        'head',
+        ['template', 'render'],
+        'renderError'
+      ]
     }]
   },
 };
