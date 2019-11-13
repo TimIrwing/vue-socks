84b82b56b3bd040c425bc435e398681775f9bd40
Tim <timirwing@gmail.com>
2019-11-13 16:03:18 +0300

    add CartItem component

---
+++ src/components/CartItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="product">
+    <img class="product__img" :src="obj.imageSrc" :alt="desc">
+    <p class="product__info">{{desc}}</p>
+    <button class="closeBtn"
+            type="button" @click="$emit('close', obj.id)"
+            aria-label="close"></button>
+  </div>
+</template>
+
+<script>
+import { capitalize } from './helpers';
+
+export default {
+  name: 'CartItem',
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    obj() {
+      return this.product.obj;
+    },
+
+    desc() {
+      return `${capitalize(this.obj.name)} - ${capitalize(this.obj.colorDesc)}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .product {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .product__img {
+    height: 5em;
+  }
+
+  .product__info {
+    width: max-content;
+  }
+</style>
